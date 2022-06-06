083dac5d80d55085994bffcad6b0b5c77c3f29a6
Bert Van Kets <bert@vankets.com>
2022-06-06 12:32:48 +0200

    trying to get SMSs to work with standalone modem. Modem resets -> needs bigger cap

--- SIM test/src/main.cpp
+++ SIM test/src/main.cpp
@@ -1,70 +1,80 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include "Adafruit_FONA.h"
 
-#define FONA_RX 3
-#define FONA_TX 2
-#define FONA_RST 4
+// Please select the corresponding model
+// #define SIM800L_IP5306_VERSION_20190610
+// #define SIM800L_AXP192_VERSION_20200327
+// #define SIM800C_AXP192_VERSION_20200609
+#define SIM800L_IP5306_VERSION_20200811
+#define SIM800L_STANDALONE
 
-#define FONA_RI_INTERRUPT 0
+// Define the serial console for debug prints, if needed
+#define DUMP_AT_COMMANDS
+#define TINY_GSM_DEBUG          Serial
 
-SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+#include "utilities.h"
 
-Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
-char pin[] = "3860";
+// Set serial for AT commands (to the module)
+SoftwareSerial SerialAT = SoftwareSerial(2,3);
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800          // Modem is SIM800
+#define TINY_GSM_RX_BUFFER      1024   // Set RX buffer to 1Kb
+
+#include <TinyGsmClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, Serial);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+// Set phone numbers, if you want to test SMS and Calls
+#define SMS_TARGET  "+32478385741"
+#define CALL_TARGET "+32478385741"
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println(F("Initializing....(May take 3 seconds)"));
-  fonaSS.begin(9600); // if you're using software serial
-  if (!fona.begin(fonaSS))
-  { // can also try fona.begin(Serial1)
-    Serial.println(F("Couldn't find FONA"));
-    while (1)
-      ;
-  }
-  Serial.println(F("FONA is OK"));
-  // fona.unlockSIM(pin);
-  // Serial.println("Waiting 10 seconds");
-  // delay(10000);
-  // Serial.print("Call status = ");
-  // Serial.println(fona.getCallStatus());
+    // Set console baud rate
+    Serial.begin(115200);
 
-  uint8_t t = fona.getNetworkStatus();
-  Serial.print(F("Network status: "));
-  if (t == 0)
-    Serial.println(F("Not registered"));
-  if (t == 1)
-    Serial.println(F("Registered (home)"));
-  if (t == 2)
-    Serial.println(F("Not registered (searching)"));
-  if (t == 3)
-    Serial.println(F("Denied"));
-  if (t == 4)
-    Serial.println(F("Unknown"));
-  if (t == 5)
-    Serial.println(F("Registered roaming"));
+    delay(10);
 
-  Serial.print("RSSI : ");
-  Serial.println(fona.getRSSI());
+    // Start power management
+    // if (setupPMU() == false) {
+    //     Serial.println("Setting power error");
+    // }
+
+    // Some start operations
+    setupModem();
+
+    // Set GSM module baud rate and UART pins
+    // SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+    SerialAT.begin(9600);
+
+    delay(6000);
 }
 
 void loop()
 {
-  // char phone[32] = {0};
-  // if(fona.incomingCallNumber(phone)){
-  //   Serial.println(F("RING!"));
-  //   Serial.print(F("Phone Number: "));
-  //   Serial.println(phone);
-  // }
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    Serial.println("Initializing modem...");
+    modem.restart();
 
-  // if (fonaSS.available())
-  // {
-  //   Serial.println(fonaSS.readString());
-  // }
-  // if (Serial.available())
-  // {
-  //   fonaSS.println(Serial.readString());
-  // }
+
+    delay(10000);
+
+    String imei = modem.getIMEI();
+    DBG("IMEI:", imei);
+
+    bool  res = modem.sendSMS(SMS_TARGET, String("Hello from ") + imei);
+    DBG("SMS:", res ? "OK" : "fail");
+
+    // Do nothing forevermore
+    while (true) {
+        modem.maintain();
+    }
 }
